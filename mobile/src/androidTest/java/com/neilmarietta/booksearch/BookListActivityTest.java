a7f5b43144040cffd982ce55756c9bb8ca134350
Neil Marietta <neil.marietta@gmail.com>
2016-06-20 19:04:37 +0200

    Add Activity recreation Test.

--- mobile/src/androidTest/java/com/neilmarietta/booksearch/BookListActivityTest.java
+++ mobile/src/androidTest/java/com/neilmarietta/booksearch/BookListActivityTest.java
@@ -1,14 +1,20 @@
 package com.neilmarietta.booksearch;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.neilmarietta.booksearch.assertion.RecyclerViewItemCountAssertion;
 import com.neilmarietta.booksearch.presentation.view.activity.BookListActivity;
+import com.neilmarietta.booksearch.util.ActivityTestRuleUtil;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -18,6 +24,21 @@ public class BookListActivityTest {
     public ActivityTestRule<BookListActivity> mActivityRule = new ActivityTestRule<>(BookListActivity.class);
 
     @Test
-    public void anyTest() {
+    public void activityReCreated() {
+        onView(withId(R.id.rv_books))
+                .check(new RecyclerViewItemCountAssertion(20));
+
+        onView(withId(R.id.rv_books)).perform(RecyclerViewActions.scrollToPosition(19))
+                .check(new RecyclerViewItemCountAssertion(40));
+
+        ActivityTestRuleUtil.recreate(mActivityRule);
+
+        onView(withId(R.id.rv_books))
+                .check(new RecyclerViewItemCountAssertion(40));
+
+        ActivityTestRuleUtil.rotateScreen(mActivityRule);
+
+        onView(withId(R.id.rv_books))
+                .check(new RecyclerViewItemCountAssertion(40));
     }
 }
